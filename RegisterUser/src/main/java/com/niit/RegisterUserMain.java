2bdbe3c938496c388412d66ea8721c776b643b41
suyash041@gmail.com <suyash041@gmail.com>
2023-04-03 15:53:44 +0000

    New Solution

---
+++ RegisterUser/src/main/java/com/niit/RegisterUserMain.java
@@ -0,0 +1,13 @@
+package com.niit;
+
+/**
+ * Hello world!
+ *
+ */
+public class RegisterUserMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
